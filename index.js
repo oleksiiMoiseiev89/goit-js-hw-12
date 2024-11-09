29837fcf267ba9071b4a66912f705e5347638328
oleksiiMoiseiev89 <alex.moiseiev.ua@gmail.com>
2024-11-09 16:53:47 +0000

    Deploying to gh-pages from @ oleksiiMoiseiev89/goit-js-hw-12@97df9d5e983f3cf4854836c8a7ff91f14ed75650 🚀

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as g,i as c,S as b}from"./assets/vendor-D73Uttp0.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const l of s.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&r(l)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();async function w(t,o,i,r){const e=new URLSearchParams({key:`${t}`,q:`${o}`,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:`${r}`,page:`${i}`});return(await g.get(`https://pixabay.com/api/?${e}`)).data}function v(t){return t.map(({webformatURL:o,largeImageURL:i,tags:r,likes:e,views:s,comments:l,downloads:y})=>`<li class="item">
+          <a href="${i}"><img class="image" src="${o}" alt="${r}" /></a>
+          <ul class="item-list">
+            <li class="item-list-item">
+              <h3 class="title">Likes</h3>
+              <p class="value">${e}</p>
+            </li>
+            <li class="item-list-item">
+              <h3 class="title">Views</h3>
+              <p class="value">${s}</p>
+            </li>
+            <li class="item-list-item">
+              <h3 class="title">Comments</h3>
+              <p class="value">${l}</p>
+            </li>
+            <li class="item-list-item">
+              <h3 class="title">Downloads</h3>
+              <p class="value">${y}</p>
+            </li>
+          </ul>
+        </li>`).join("")}const L="46849088-f4247fdb1b297b5b75b98a0ef",d=document.querySelector(".form"),u=document.querySelector(".list"),a=document.querySelector(".loadMoreBtn"),m=document.querySelector(".loader"),h=15;let n=1,p="";d.addEventListener("submit",$);a.addEventListener("click",S);function $(t){a.style.display="none",u.innerHTML="",p=t.target.elements.input.value.trim(),t.preventDefault(),p!=0&&(m.style.display="block",n=1,f())}function S(){m.style.display="block",n+=1,f()}function f(){w(L,p,n,h).then(t=>{const o=t.hits;o.length!==0?(u.insertAdjacentHTML("beforeend",v(o)),t.totalHits>h*n?a.style.display="block":(a.style.display="none",c.show({class:"toast",position:"topRight",messageColor:"white",message:"We're sorry, but you've reached the end of search results."})),new b(".list a",{captions:!0,captionType:"attr",captionsData:"alt",captionPosition:"bottom",captionDelay:250}).refresh()):(m.style.display="none",c.show({class:"toast",position:"topRight",icon:"icon",iconUrl:err,iconColor:"white",messageColor:"white",message:"Sorry, there are no images matching your search query. Please try again!"}))}).catch(t=>{c.show({class:"toast",position:"topRight",messageColor:"white",title:"Error",titleColor:"white",message:"Please try again!"}),t.response?console.error("Server error:",t.response.status):t.request?console.error("No response from server"):console.error("Unknown error:",t.message)}).finally(()=>{const t=u.lastChild.getBoundingClientRect();scrollBy({top:t.top+t.height*2,behavior:"smooth"})}),d.reset()}
 //# sourceMappingURL=index.js.map
